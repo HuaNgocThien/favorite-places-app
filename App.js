8b026c73a7c9e3b55484fe2c992956af28742992
Ngoc Thien <huangocthien2510@gmail.com>
2025-04-15 03:52:33 +0700

    feat: add navigation and IconButton component

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import AllPlaces from "./src/screens/AllPlaces";
 import AddPlace from "./src/screens/AddPlace";
+import IconButton from "./src/components/UI/IconButton";
 
 const Stack = createStackNavigator();
 
@@ -12,7 +13,22 @@ export default function App() {
       <StatusBar />
       <NavigationContainer>
         <Stack.Navigator initialRouteName="AllPlaces">
-          <Stack.Screen name="AllPlaces" component={AllPlaces} />
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add"
+                  color={tintColor}
+                  size={24}
+                  onPress={() => {
+                    navigation.navigate("AddPlace");
+                  }}
+                />
+              ),
+            })}
+          />
           <Stack.Screen name="AddPlace" component={AddPlace} />
         </Stack.Navigator>
       </NavigationContainer>
